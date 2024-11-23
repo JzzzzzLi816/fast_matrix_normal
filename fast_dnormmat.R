a6cbed03882e9a989592c52ca7bafa0c231e28c1
Zachary Apell <zapell@umich.edu>
2024-11-23 12:59:25 -0500

    Update fast_dnormmat.R
    pdf working for cholesky, added checks for sym, pos def

--- fast_dnormmat.R
+++ fast_dnormmat.R
@@ -1,4 +1,4 @@
-#fast_dmatnorm <- function(Z, M, U, V, log=TRUE, Precision=FALSE) {
+fast_dmatnorm <- function(Z, M, U, V, log=TRUE, Precision=FALSE, tol=1e-8) {
   if(!is.null(M)){
     n <- nrow(M)
     p <- ncol(M)
@@ -21,22 +21,24 @@
   # U,v square
   #  M nxp
   # check for nas
+  # cholesky check - pos definite
+  check_matnorm(M,U,V,tol)
   
-  #cholesky check?
   
   Ru <- chol(U)
   Rv <- chol(V)
   
   # solving for A
   D <- Z - M 
-  #print(dim(D))
   A2 = forwardsolve(Ru, D, upper.tri=TRUE,transpose=TRUE)
   
-  #A = backsolve(Rv, A2)
-  A = A2 %*% chol2inv(Rv)
+  A = t(forwardsolve(t(Rv), t(A2)))
+  
+  #A = A2 %*% solve(Rv)
   # calculate log density first
   # rcpp?
   numerator = -0.5 * sum(A^2)
+  # denom correct and matches
   denom = (n*p/2)*log(2*pi) + sum(n*log(diag(Rv))) + sum(p*log(diag(Ru)))
   
   log.dens = numerator - denom
@@ -46,7 +48,7 @@
   } else {
     return(exp(log.dens))
   }
-#}
+}
 
 
 set.seed(123)
@@ -56,38 +58,67 @@ V <- matrix(c(87, 13, 13, 112), nrow = 2, ncol = 2, byrow = TRUE)
 #' V # Right covariance matrix (2 x 2), say the covariance between parameters.
 U <- diag(10) # Block of left-covariance matrix ( 84 x 84), say the covariance between subjects.
 
+#system.time(
 fast_dmatnorm(A, M, U, V)
+#)
 fast_dmatnorm(A, M, U, V, log = FALSE)
 
-
+# original matrixnorm
+#system.time(
 dmatnorm(A, M, U, V)
+#)
 dmatnorm(A, M, U, V, log = FALSE)
 
-# matrix norm code
-dmatnorm <- function(
-    A,
-    M,
-    U,
-    V,
-    tol = .Machine$double.eps^0.5,
-    log = TRUE
-) {
-  n <- nrow(A)
-  p <- ncol(A)
+
+is.sym <- function(X, tol=1e-8) {
+  if (dim(X)[1] != dim(X)[2]) {FALSE}
   
-  # Checks
-  if (is.data.frame(A)) A <- as.matrix(A)
-  #if (sum(dim(A) == dim(M)) != 2) stop("M must have same dimensions as A.")
-  #check_matnorm(s = 1, M, U, V, tol)
-  
-  # The Log Density
-  log.dens <- (-n * p / 2) * log(2 * pi) - p / 2 * log(det(U)) - n / 2 * log(det(V)) +
-    -1 / 2 * tr(solve(U) %*% (A - M) %*% solve(V) %*% t(A - M))
-  
-  # Return
-  if (log) {
-    return(log.dens)
-  } else {
-    return(exp(log.dens))
+  else if (all(abs(X-t(X)) < tol)) {TRUE}
+  else {FALSE}
+}
+
+
+
+is.pos.def <- function(X, tol=1e-8) {
+  eigenvalues <- eigen(X, symmetric = TRUE, only.values = TRUE)$values
+  if (min(eigenvalues) > tol) {TRUE}
+  else {FALSE}
+}
+
+
+# MATRIX CHECK
+check_matnorm <- function(M, U, V, tol=1e-8) {
+  if (anyNA(M)) {
+    stop("M contains missing values.", call. = FALSE)
   }
+  if (anyNA(U)) {
+    stop("U contains missing values.")
+  }
+  if (anyNA(V)) {
+    stop("V contains missing values.")
+  }
+  if (nrow(M) != nrow(U)) {
+    stop("The mean matrix M has different sample size than the scale sample size
+         matrix U. M has ", dim(M)[[1]], "rows, and U has ", dim(U)[[1]], ".")
+  }
+  if (ncol(M) != nrow(V)) {
+    stop("The mean matrix M has different number of parameters than scale
+         parameter matrix V: M  -- ", dim(M)[2], "; V -- ", dim(V)[1], ".")
+  }
+  if (!is.sym(U, tol)) {
+    stop("U is not symmetric.")
+  }
+  if (!is.sym(V, tol)) {
+    stop("V is not symmetric.")
+  }
+  
+  if (!is.pos.def(U, tol)) {
+    stop("U is not positive definite. Calculation may not be accurate.
+         Possibly lower tolerance.")
+  }
+  if (!is.pos.def(V, tol)) {
+    stop("V is not positive definite. Calculation may not be accurate.
+         Possibly lower tolerance.")
+  }
+  return(invisible())
 }
